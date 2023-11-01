64dc6a1062e9a1a7201616ad7e3526f30c27b907
nesterch <terlemez.neslihan@gmail.com>
2023-11-01 17:34:01 +0100

    RequestSpecification eklendi

--- src/test/java/GoRest/_06_GoRestUsersTest.java
+++ src/test/java/GoRest/_06_GoRestUsersTest.java
@@ -2,13 +2,17 @@ package GoRest;
 
 import Model.User;
 import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class _06_GoRestUsersTest {
 
@@ -24,7 +28,7 @@ public class _06_GoRestUsersTest {
 //    }
 //
 //    Authorization
-//    Bearer 7855808b298b71ca4af40039bc33e831f19046ece1a1f05c6ca13fba632b82e6
+//    Bearer 787c83039875452ce6a32a7b73e7a10c1d4443273522652da0f13d5e76a27713
 //
 //
 //    dönüşte 201
@@ -32,8 +36,21 @@ public class _06_GoRestUsersTest {
 
     Faker randomUreteci=new Faker();
     int userID=0;
+    RequestSpecification reqSpec;
 
-    @Test
+    @BeforeClass
+    public void setup(){
+
+        baseURI="https://gorest.co.in/public/v2/users";
+
+        reqSpec = new RequestSpecBuilder()
+                .addHeader("Authorization","Bearer 787c83039875452ce6a32a7b73e7a10c1d4443273522652da0f13d5e76a27713")
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+
+
+    @Test(enabled = false)
     public void createUserJSon(){
 
         String rndFullName= randomUreteci.name().fullName();
@@ -70,6 +87,35 @@ public class _06_GoRestUsersTest {
 
         userID=
                 given() // giden body, token, contentType
+                        .spec(reqSpec)
+                        .body(newUser) // giden body
+                        .contentType(ContentType.JSON)
+
+                        .when()
+                        .post("")
+
+                        .then()
+                        .log().body()
+                        .statusCode(201)
+                        .extract().path("id");
+        ;
+        System.out.println("userID = " + userID);
+    }
+
+    @Test(enabled = false)
+    public void createUserClass(){
+
+        String rndFullName= randomUreteci.name().fullName();
+        String rndEmail= randomUreteci.internet().emailAddress();
+
+        User newUser=new User();
+        newUser.name=rndFullName;
+        newUser.email=rndEmail;
+        newUser.gender="male";
+        newUser.status="active";
+
+        userID=
+                given() // giden body, token, contentType
                         .header("Authorization","Bearer 787c83039875452ce6a32a7b73e7a10c1d4443273522652da0f13d5e76a27713")
                         .body(newUser) // giden body
                         .contentType(ContentType.JSON)
@@ -85,35 +131,23 @@ public class _06_GoRestUsersTest {
         System.out.println("userID = " + userID);
     }
 
-    @Test
-    public void createUserClass(){
 
-        String rndFullName= randomUreteci.name().fullName();
-        String rndEmail= randomUreteci.internet().emailAddress();
+    @Test(dependsOnMethods = "createUserMAP")
+    public void getUserById(){
 
-        User newUser=new User();
-        newUser.name=rndFullName;
-        newUser.email=rndEmail;
-        newUser.gender="male";
-        newUser.status="active";
+        given()
+                .spec(reqSpec)
 
-        userID=
-                given() // giden body, token, contentType
-                        .header("Authorization","Bearer 7855808b298b71ca4af40039bc33e831f19046ece1a1f05c6ca13fba632b82e6")
-                        .body(newUser) // giden body
-                        .contentType(ContentType.JSON)
+                .when()
+                .get(""+userID)
 
-                        .when()
-                        .post("https://gorest.co.in/public/v2/users")
-
-                        .then()
-                        .log().body()
-                        .statusCode(201)
-                        .extract().path("id");
+                .then()
+                .log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id", equalTo(userID))
         ;
-        System.out.println("userID = " + userID);
     }
-
 
 
 
